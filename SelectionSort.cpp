a0d60b42108d4a99fcb1509f695930184a1a66d1
Bogdan Jidovu <bogdanjidovu98@gmail.com>
2019-10-13 16:01:29 +0300

    Selection Sort in C++

---
+++ SelectionSort.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void selectSort(int* v, int n) {
+	int min, ct;
+	for (int i = 0; i < n - 1; i++) {
+		min = v[i];
+		ct = i;
+		for(int j = i + 1; j < n; j++) {
+			if (min > v[j]) {
+				min = v[j];
+				ct = j;
+			}
+		}
+		if (ct != i) {
+			swap(v[i], v[ct]);
+		}
+	}
+}
+
+void print(int* v, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << v[i] << " ";
+	}
+	cout << "\n";
+}
+
+int main() {
+	int n, *v;
+	v = new int[n];
+
+	cout << "Give the number of elements: ";
+	cin >> n;
+
+	cout << "Give the numbers: ";
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	selectSort(v, n);
+
+	print(v, n);
+
+	delete[] v;
+	return 0;
+}
